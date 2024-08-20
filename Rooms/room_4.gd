a26af9919fbd24e99e2f129aba12375b5b313d00
Nikita Plotnikov 221RDB021 <67636595+Gallenmar@users.noreply.github.com>
2024-08-20 19:20:55 +0300

    last room done

--- Rooms/room_4.gd
+++ Rooms/room_4.gd
@@ -1,5 +1,10 @@
 extends WorldParent
 
+@export var mob_scene: PackedScene
+@export var killer_scene: PackedScene
+@export var bcell_scene: PackedScene
+@onready var player = $Player
+
 var entered = false
 
 func _ready():
@@ -8,9 +13,26 @@ func _ready():
 
 func _on_entrance_body_exited(body):
 	if !entered:
+		var mob = mob_scene.instantiate()
+		mob.global_position = $"Spawn Locations/Marker2D".global_position
+		mob.player = player
+		add_child(mob)
+		var mob2 = mob_scene.instantiate()
+		mob2.global_position = $"Spawn Locations/Marker2D2".global_position
+		mob2.player = player
+		add_child(mob2)
+		var mob3 = mob_scene.instantiate()
+		mob3.global_position = $"Spawn Locations/Marker2D3".global_position
+		mob3.player = player
+		add_child(mob3)
 		
+		var bcell = bcell_scene.instantiate()
+		bcell.global_position = $"Spawn Locations/bcell".global_position
+		add_child(bcell)
 		
-		
+		var killer = killer_scene.instantiate()
+		killer.global_position = $"Spawn Locations/killer".global_position
+		add_child(killer)
 		connect_enemies()
 		entered = true
 
@@ -19,6 +41,7 @@ func _process(_delta):
 	if get_tree().get_nodes_in_group("Enemies").size() <=0:
 		$UI/ColorRect.show()
 		$UI/Label.show()
+		get_tree().paused = true
 
 
 
