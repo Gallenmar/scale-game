53f799cedb74c456749e4551e91184d21bf5e171
Nikita Plotnikov 221RDB021 <67636595+Gallenmar@users.noreply.github.com>
2024-08-20 13:25:50 +0300

    b cell crush

--- Mobs/Killer/killer_cell.gd
+++ Mobs/Killer/killer_cell.gd
@@ -3,8 +3,19 @@ extends CharacterBody2D
 var player_nearby :bool = false
 var can_shoot : bool = true
 var health = 40
+#crush
+var player_near = true
+var hovered = false
+signal dead(pos:Vector2)
+signal crush(pos)
+#end crush
 
 signal shoot(pos, direction)
+
+func _ready():
+	#crush
+	$LowHealth.hide()
+	$Border.hide()
 
 func _process(_delta):
 	if(player_nearby):
@@ -15,6 +26,12 @@ func _process(_delta):
 			shoot.emit(pos, dir)
 			can_shoot = false
 			$ShootCooldown.start()
+	#crush
+	if hovered and health <=10 and player_near:
+		$Border.show()
+	else:
+		$Border.hide()
+	#end crush
 
 func _on_attack_area_body_entered(_body):
 	player_nearby = true
@@ -27,7 +44,41 @@ func _on_attack_area_body_exited(_body):
 func _on_shoot_cooldown_timeout():
 	can_shoot = true
 
+#crush
 func take_damage():
 	health -= 10
+	if health <=10:
+		#$Sprite2D.material.set_shader_parameter("progress", 1)
+		$LowHealth.show()
 	if health<=0:
+		dead.emit(global_position)
 		queue_free()
+
+func jump_away(dir):
+	var tween = get_tree().create_tween()
+	tween.tween_property($".", "global_position", global_position+ dir*200 ,0.2)
+
+func _on_hover_area_input_event(viewport, event, shape_idx):
+	if Input.is_action_just_pressed("dash") and $Border.visible:
+		crush.emit(global_position)
+		queue_free()
+		dead.emit(global_position)
+
+
+func _on_hover_area_mouse_entered():
+	hovered = true
+
+
+func _on_hover_area_mouse_exited():
+	hovered = false
+
+
+
+func _on_crush_distance_body_entered(body):
+	player_near = true
+
+
+func _on_crush_distance_body_exited(body):
+	player_near = false
+
+#end crush
--- World/world.gd
+++ World/world.gd
@@ -12,15 +12,23 @@ var item_chance = item_def_chance
 func _ready():
 	for i in get_tree().get_nodes_in_group("Killers"):
 		i.connect('shoot', _on_killer_shoot)
+	for i in get_tree().get_nodes_in_group("Killers"):
+		i.connect('dead', _on_enemy_dead)
+	for i in get_tree().get_nodes_in_group("Killers"):
+		i.connect('crush', _on_enemy_crush)
 	for i in get_tree().get_nodes_in_group("Mobs"):
-		i.connect('dead', _on_mob_dead)
+		i.connect('dead', _on_enemy_dead)
 	for i in get_tree().get_nodes_in_group("Mobs"):
-		i.connect('crush', _on_mob_crush)
+		i.connect('crush', _on_enemy_crush)
 	for i in get_tree().get_nodes_in_group("Bcells"):
 		i.connect('shoot', _on_bcell_shoot)
+	for i in get_tree().get_nodes_in_group("Bcells"):
+		i.connect('dead', _on_enemy_dead)
+	for i in get_tree().get_nodes_in_group("Bcells"):
+		i.connect('crush', _on_enemy_crush)
 	for i in get_tree().get_nodes_in_group("Boss"):
 		i.connect('shoot', _on_boss_shoot)
-
+	
 func _on_boss_shoot(pos, dir):
 	create_boss_bullet(pos, dir)
 
@@ -30,14 +38,14 @@ func _on_bcell_shoot(pos, dir):
 func _on_killer_shoot(pos, dir):
 	create_bullet(pos, dir)
 
-func _on_mob_dead(pos):
+func _on_enemy_dead(pos):
 	if randi() % 100 >=item_chance: #chanse 50%
 		var item = ITEM.instantiate() as Area2D
 		item.position = pos
 		$Items.call_deferred("add_child", item)
 		item_chance = item_def_chance
 
-func _on_mob_crush(pos):
+func _on_enemy_crush(pos):
 	Globals.is_vulnurable = false
 	$Player.dash_start(pos)
 	var tween = get_tree().create_tween()
